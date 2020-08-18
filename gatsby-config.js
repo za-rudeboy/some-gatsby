bef0ebc31de24e3e0dfe1c180a180583262dc824
rudy.adams <rudy.adams@investec.co.za>
2020-08-18 22:33:16 +0200

    With my background

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,19 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
-}
+  plugins: [
+    {
+      resolve: "gatsby-plugin-chakra-ui",
+      options: {
+        /**
+         * @property {boolean} [isResettingCSS=true]
+         * if false, this plugin will not use `<CSSReset />
+         */
+        isResettingCSS: true,
+        /**
+         * @property {boolean} [isUsingColorMode=true]
+         * if false, this plugin will not use <ColorModeProvider />
+         */
+        isUsingColorMode: false,
+      },
+    },
+  ],
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,23 @@
 import React from "react"
+import { Box, Text, Button } from "@chakra-ui/core"
+import Layout from "../components/layout"
+import macImage from "../../assets/front-page-image.jpg"
 
+console.log(macImage)
+const imageStyle = {
+  imageContainer : {
+    backgroundImage: `url(${macImage})`,
+    backgroundSize: "cover"
+  }
+}
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <Box
+      width="100vw"
+      minHeight="100vh"
+      style = {imageStyle.imageContainer}
+    >
+      <Text>Boom</Text>
+    </Box>
+  )
 }
