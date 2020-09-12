40230c813c7ad54ef53bf5f1eebd4ecb6cca9c70
rudy.adams <rudy.adams@investec.co.za>
2020-09-12 23:23:32 +0200

    Some optimisations

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Box, Heading } from "@chakra-ui/core"
-import macImage from "../../assets/front-page-image.jpg"
+import macImage from "../../assets/front-page-image.webp"
 import Header from "../components/header"
 
 const imageStyle = {
@@ -14,7 +14,7 @@ export default function Home() {
   return (
     <Box width="100vw" minHeight="100vh" style={imageStyle.imageContainer}>
       <Header></Header>
-      <Heading marginLeft="100px" marginTop="30px" color="#c0d8caf5" as="h5">
+      <Heading marginLeft="1rem" marginTop="30px" color="#c0d8caf5" as="h5">
         Rudy Adams
       </Heading>
     </Box>
