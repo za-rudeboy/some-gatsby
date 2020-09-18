8c1a46d33933d096333905bf9e2405256b9ffa57
rudy.adams <rudy.adams@investec.co.za>
2020-09-18 21:30:57 +0200

    Added background color to image coz it takes forever to load

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,21 +2,23 @@ import React from "react"
 import { Box, Heading } from "@chakra-ui/core"
 import macImage from "../../assets/front-page-image.jpg"
 import Header from "../components/header"
+import { ThemeProvider } from "emotion-theming"
+import theme from "../gatsby-plugin-chakra-ui/theme"
 
 const imageStyle = {
   imageContainer: {
     backgroundImage: `url(${macImage})`,
     backgroundSize: "cover",
-    backgroundColor: "darkJungleGreen",
+    backgroundColor: `${theme.colors.darkJungleGreen}`,
   },
 }
 export default function Home() {
   return (
-    <Box width="100vw" minHeight="100vh" style={imageStyle.imageContainer}>
-      <Header></Header>
-      <Heading marginLeft="1rem" marginTop="30px" color="#c0d8caf5" as="h5">
-        Rudy Adams
-      </Heading>
-    </Box>
+      <Box width="100vw" minHeight="100vh" style={imageStyle.imageContainer}>
+        <Header></Header>
+        <Heading marginLeft="1rem" marginTop="30px" color="#c0d8caf5" as="h5">
+          Rudy Adams
+        </Heading>
+      </Box>
   )
 }
